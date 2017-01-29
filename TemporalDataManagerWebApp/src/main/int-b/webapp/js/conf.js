a5d9687690247c1c6d6c22ef21f02237aa95098d
ftoral <fabien.toral@c-s.fr>
2017-01-29 19:09:10 +0100

    [#152736] Nouveau target pour INT-B

---
+++ TemporalDataManagerWebApp/src/main/int-b/webapp/js/conf.js
@@ -0,0 +1,9 @@
+/**
+ * This file contains global configuration to be used by TSviz module.
+ */
+
+
+/**
+ * Root path to TS database API
+ */
+var TSDBroot="http://172.28.15.13/TemporalDataManagerWebApp/webapi";
