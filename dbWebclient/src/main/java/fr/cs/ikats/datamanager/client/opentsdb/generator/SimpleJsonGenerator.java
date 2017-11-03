31b9a096bed299e38c0afe4e107d08856e686caf
ftortora <fabien.tortora@c-s.fr>
2017-11-03 10:41:25 +0100

    [#163207] Applying to all code base
    Signed-off-by: ftortora <fabien.tortora@c-s.fr>

--- dbWebclient/src/main/java/fr/cs/ikats/datamanager/client/opentsdb/generator/SimpleJsonGenerator.java
+++ dbWebclient/src/main/java/fr/cs/ikats/datamanager/client/opentsdb/generator/SimpleJsonGenerator.java
@@ -1,6 +1,30 @@
 /**
+ * LICENSE:
+ * --------
+ * Copyright 2017 CS SYSTEMES D'INFORMATION
+ * 
+ * Licensed to CS SYSTEMES D'INFORMATION under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. CS licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * 
+ * @author Fabien TORAL <fabien.toral@c-s.fr>
+ * @author Fabien TORTORA <fabien.tortora@c-s.fr>
  * 
  */
+
 package fr.cs.ikats.datamanager.client.opentsdb.generator;
 
 import java.util.ArrayList;
@@ -16,7 +40,6 @@ import fr.cs.ikats.datamanager.DataManagerException;
 /**
  * generate a Json import request from a list of points
  * 
- * @author ikats
  * 
  */
 public class SimpleJsonGenerator {
@@ -331,3 +354,4 @@ public class SimpleJsonGenerator {
     // point.addTag(splittedLine[POSITION_NOM_TAG],
     // splittedLine[POSITION_VAL_TAG]);
 }
+
--- dbWebclient/src/main/java/fr/cs/ikats/datamanager/client/opentsdb/generator/SplittedLineReader.java
+++ dbWebclient/src/main/java/fr/cs/ikats/datamanager/client/opentsdb/generator/SplittedLineReader.java
@@ -1,3 +1,30 @@
+/**
+ * LICENSE:
+ * --------
+ * Copyright 2017 CS SYSTEMES D'INFORMATION
+ * 
+ * Licensed to CS SYSTEMES D'INFORMATION under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. CS licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * 
+ * @author Fabien TORAL <fabien.toral@c-s.fr>
+ * @author Fabien TORTORA <fabien.tortora@c-s.fr>
+ * 
+ */
+
 package fr.cs.ikats.datamanager.client.opentsdb.generator;
 
 import java.text.ParseException;
@@ -9,7 +36,6 @@ import fr.cs.ikats.datamanager.client.opentsdb.generator.ReaderConfiguration.Col
 /**
  * to read splitted line from csv input file according to columns configuration
  * 
- * @author ikats
  *
  */
 public class SplittedLineReader {
@@ -68,3 +94,4 @@ public class SplittedLineReader {
     }
     
 }
+
