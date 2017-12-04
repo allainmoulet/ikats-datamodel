e3c8e6bb02ca64c5a1745ed1398ce64869281fe0
ftortora <fabien.tortora@c-s.fr>
2017-12-04 07:48:26 +0100

    [#160900] getByName added and getById improved

--- DataBaseCommon/src/main/java/fr/cs/ikats/table/TableDAO.java
+++ DataBaseCommon/src/main/java/fr/cs/ikats/table/TableDAO.java
@@ -2,7 +2,7 @@
  * LICENSE:
  * --------
  * Copyright 2017 CS SYSTEMES D'INFORMATION
- * 
+ *
  * Licensed to CS SYSTEMES D'INFORMATION under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -10,19 +10,19 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  * @author Fabien TORTORA <fabien.tortora@c-s.fr>
  * @author Fabien TORAL <fabien.toral@c-s.fr>
- * 
+ *
  */
 
 package fr.cs.ikats.table;
@@ -51,11 +51,11 @@ public class TableDAO extends DataBaseDAO {
     private static final Logger LOGGER = Logger.getLogger(TableDAO.class);
 
     private DatabaseManager instance;
-    
+
     public TableDAO() {
         instance = DatabaseManager.getInstance();
     }
-    
+
     /* (non-Javadoc)
      * @see fr.cs.ikats.common.dao.DataBaseDAO#getSession()
      */
@@ -145,33 +145,34 @@ public class TableDAO extends DataBaseDAO {
         return result;
     }
 
+
     /**
      * Get a TableEntity by providing its id (which is unique)
      *
-     * @param name unique name of the TableEntity to get
+     * @param id unique id of the TableEntity to get
      *
-     * @return The TableEntity matching this name
+     * @return The TableEntity matching this id
      *
      * @throws IkatsDaoMissingRessource if there is no TableEntity matching the id
      */
-    public TableEntity getByName(String name) throws IkatsDaoMissingRessource {
+    public TableEntity getById(Integer id) throws IkatsDaoMissingRessource {
         TableEntity result = null;
 
         Session session = getSession();
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
-            
-            Query query = session.createQuery("from TableEntity fetch all properties where name = :name");
-            query.setParameter("name", name);
+
+            Query query = session.createQuery("from TableEntity fetch all properties where id = :id");
+            query.setParameter(id, id);
             result = (TableEntity) query.uniqueResult();
 
             if (result == null) {
-                String msg = "Searching table from name=" + name + ": no resource found, but should exist.";
+                String msg = "Table " + id + " not found";
                 LOGGER.error(msg);
                 tx.rollback();
                 throw new IkatsDaoMissingRessource(msg);
-            } 
+            }
             else {
                 tx.commit();
             }
@@ -190,6 +191,54 @@ public class TableDAO extends DataBaseDAO {
 
         return result;
     }
+
+
+    /**
+     * Get a TableEntity by providing its name (which is unique)
+     *
+     * @param name unique name of the TableEntity to get
+     *
+     * @return The TableEntity matching this name
+     *
+     * @throws IkatsDaoMissingRessource if there is no TableEntity matching the name
+     */
+    public TableEntity getByName(String name) throws IkatsDaoMissingRessource {
+        TableEntity result = null;
+
+        Session session = getSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+
+            Query query = session.createQuery("from TableEntity fetch all properties where name = :name");
+            query.setParameter("name", name);
+            result = (TableEntity) query.uniqueResult();
+
+            if (result == null) {
+                String msg = "Table " + name + " not found";
+                LOGGER.error(msg);
+                tx.rollback();
+                throw new IkatsDaoMissingRessource(msg);
+            }
+            else {
+                tx.commit();
+            }
+
+        }
+        catch (RuntimeException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            // Re-raise the original exception
+            throw e;
+        }
+        finally {
+            session.close();
+        }
+
+        return result;
+    }
+
 
     /**
      * Save a table
