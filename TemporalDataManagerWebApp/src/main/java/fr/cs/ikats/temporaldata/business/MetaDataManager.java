74bbd26f1254f66e67ae5ebcd7c1dcbbe6f15f15
ftortora <fabien.tortora@c-s.fr>
2017-12-07 09:40:14 +0100

    [#165412] Changing Error message when conflict during metadata save

--- TemporalDataManagerWebApp/src/main/java/fr/cs/ikats/temporaldata/business/MetaDataManager.java
+++ TemporalDataManagerWebApp/src/main/java/fr/cs/ikats/temporaldata/business/MetaDataManager.java
@@ -2,7 +2,7 @@
  * LICENSE:
  * --------
  * Copyright 2017 CS SYSTEMES D'INFORMATION
- * 
+ * <p>
  * Licensed to CS SYSTEMES D'INFORMATION under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -10,21 +10,20 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  * @author Fabien TORAL <fabien.toral@c-s.fr>
  * @author Fabien TORTORA <fabien.tortora@c-s.fr>
  * @author Mathieu BERAUD <mathieu.beraud@c-s.fr>
  * @author Maxime PERELMUTER <maxime.perelmuter@c-s.fr>
- * 
  */
 
 package fr.cs.ikats.temporaldata.business;
@@ -38,6 +37,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.apache.log4j.Logger;
 
 import fr.cs.ikats.common.dao.exception.IkatsDaoConflictException;
 import fr.cs.ikats.common.dao.exception.IkatsDaoException;
@@ -57,7 +58,6 @@ import fr.cs.ikats.temporaldata.application.TemporalDataApplication;
 import fr.cs.ikats.temporaldata.business.table.TableManager;
 import fr.cs.ikats.temporaldata.exception.IkatsException;
 import fr.cs.ikats.temporaldata.exception.ResourceNotFoundException;
-import org.apache.log4j.Logger;
 
 /**
  * MetaData Business Manager singleton.
@@ -193,7 +193,7 @@ public class MetaDataManager {
             // Dealing with the first error ...
 
             IkatsException contextError = new IkatsException(
-                    "persistMetaData() : import has globally failed with one transaction at DAO level: see server logs.", daoException);
+                    "At least 1 metadata could be imported because it already exist", daoException);
 
             logger.error(contextError);
             throw contextError;
