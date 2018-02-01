f399e27b6fe55e916846310d7d33944fbae1b109
ftoral <fabien.toral@c-s.fr>
2018-02-01 18:09:57 +0100

    [#169202] Remove obsolete target wrt to templated configuration for container

--- TemporalDataManagerWebApp/src/main/docker/webapp/js/conf.js
+++
@@ -1,9 +0,0 @@
-/**
- * This file contains global configuration to be used by TSviz module.
- */
-
-
-/**
- * Root path to TS database API
- */
-var TSDBroot="http://localhost:8180/TemporalDataManagerWebApp/webapi";
