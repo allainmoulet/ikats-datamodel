3f9eaf161a7ccf119acf68ee2b7690afd66b6e2c
ftoral <fabien.toral@c-s.fr>
2018-02-20 23:10:42 +0100

    [#169209] Update the script to be sh (posix) compliant)

--- script/inject_configuration.sh
+++ script/inject_configuration.sh
@@ -1,54 +1,48 @@
-#! /bin/bash -
+#! /bin/sh -
 
 set -xe
 
-function test_for_variable {
+test_for_variable() {
   vname=$1
-  vvalue=${!vname}
-  default_value=$2
+  eval "vvalue=\$$vname"
 
   if [ -z "$vvalue" ]
   then
-    if [ -z "$2" ]
-    then
-      # No fallback has been given to the function
-      echo "The environment variable ${vname} must have a value"
-      exit 1
-    else
-      # Setting the variable value to the provided default
-      eval $vname=$default_value
-    fi
+    # No fallback has been given to the function
+	echo "The environment variable ${vname} must have a value"
+	exit 1
   fi
 }
 
-function replace_variable_in {
+replace_variable_in() {
   #Usage: replace_variable_in VARIABLE_NAME file1 file2 ...
   vname=$1
-  vvalue=${!vname}
+  eval "vvalue=\$$vname"
 
   shift
   sed -i "s/{${vname}}/${vvalue}/g" $@
 }
 
-function fill_variables_in_war {
+fill_variables_in_war() {
   echo "Unpacking the .war"
-  tmp_dir=$(dirname $WAR_PATH)/tmp
+  tmp_dir=/tmp/exploded_war
   war_file=$(basename $WAR_PATH)
   mkdir -p $tmp_dir && cd $tmp_dir
 
-  jar xf ../$war_file
+  unzip -qo $WAR_PATH
 
-  echo "Setting environment variables"
-  for v in "${env_variables[@]}"
+  echo "Setting environment variables: $@"
+  for v in "$@" 
   do
     test_for_variable $v
     replace_variable_in $v WEB-INF/classes/*.*
   done
   echo "repacking the .war"
 
-  jar cf ../$war_file . \
-    && cd .. \
-    && rm -rf tmp
+  zip -qr ../$war_file . \
+    && cd - \
+    && mv $war_file $WAR_PATH \
+    && rm -rf $tmp_dir
 }
 
 if [ "$#" -ne 1 ]
@@ -59,19 +53,17 @@ fi
 
 WAR_PATH=$1
 
-env_variables=(
-  "DB_HOST"
-  "DB_PORT"
-  "TSDB_HOST"
-  "TSDB_PORT"
-  "C3P0_ACQUIRE_INCREMENT"
-  "C3P0_MAX_SIZE"
-  "C3P0_IDLE_TEST_PERIOD"
-  "C3P0_MAX_STATEMENTS"
-  "C3P0_MIN_SIZE"
-  "C3P0_TIMEOUT"
-)
+eval set -- "DB_HOST \
+	DB_PORT \
+	TSDB_HOST \
+	TSDB_PORT \
+	C3P0_ACQUIRE_INCREMENT \
+	C3P0_MAX_SIZE \
+	C3P0_IDLE_TEST_PERIOD \
+	C3P0_MAX_STATEMENTS \
+	C3P0_MIN_SIZE \
+	C3P0_TIMEOUT"
 
-# Application configuration
-fill_variables_in_war
+# Apply configuration
+fill_variables_in_war $@
 
